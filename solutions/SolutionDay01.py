1da43cd092a1be7f1a12f546496159d5c713bfa3
Menno Steensma <menno.steensma@heliox-energy.com>
2022-12-01 21:48:54 +0100

    small rfactor

--- solutions/SolutionDay01.py
+++ solutions/SolutionDay01.py
@@ -1,21 +1,33 @@
-
 from utils.AocUtils import *
 from utils.FileReader import *
 
-
-    
 class CalorieCalculator:
+    
+    def __init__(self):
+        self.calArray = []
+        
+    def sumNBiggestEaters(self, inputCals, n):
+        self.calculateMaxCalories(inputCals)
+        self.calArray.sort(reverse=True)
+        return sum(self.calArray[:n])
+
     def calculateMaxCalories(self, inputCals):
         maxCal = 0
         tmpVal = 0
+        self.calArray = []
         
         for cal in inputCals:
             tmpVal += cal
             if cal == 0:
+                self.calArray.append(tmpVal)
                 if tmpVal > maxCal:
                     maxCal = tmpVal
                 tmpVal = 0
-                
+              
+        self.calArray.append(tmpVal)  
+        if tmpVal > maxCal:
+            maxCal = tmpVal
+                    
         return maxCal
                 
         
@@ -24,12 +36,7 @@ def solutionDay01():
     fileReader = FileReader()
     calorieCalculator = CalorieCalculator()
     inputArray = fileReader.readLinesToIntArray("input/inputday1")
-    answer = calorieCalculator.calculateMaxCalories(inputArray)
-    print(answer)
+    answer1 = calorieCalculator.sumNBiggestEaters(inputArray, 1)
+    answer2 = calorieCalculator.sumNBiggestEaters(inputArray, 3)
+    printAnswer(1, answer1, answer2)
     
-    
-    # CalorieCalculator.calculateCaloriesElf(inputArray)
-    
-    # printAnswer(1, answerPart1, answerPart2)
-    
-solutionDay01()
--- test_unittests.py
+++ test_unittests.py
@@ -10,11 +10,29 @@ class Test_CalorieCalculator(unittest.TestCase):
         self.calorieCalculator = CalorieCalculator()
         self.fileReader = FileReader()
     
-    def test_CalculateDepthIncreases_Part1_Example1(self):
+    def test_CalculateMaxCaloriesInputUT(self):
         calories = self.fileReader.readLinesToIntArray("input_ut/inputday1_ut")
         
-        maxCalories = self.calorieCalculator.calculateMaxCalories(calories)
+        maxCalories = self.calorieCalculator.sumNBiggestEaters(calories, 1)
         self.assertEqual(maxCalories, 24000)
+
+    def test_SumBiggestThreeEatersInputUT(self):
+        calories = self.fileReader.readLinesToIntArray("input_ut/inputday1_ut")
+        
+        maxCalories = self.calorieCalculator.sumNBiggestEaters(calories, 3)
+        self.assertEqual(maxCalories, 45000)
+
+    def test_CalculateMaxCaloriesInput(self):
+        calories = self.fileReader.readLinesToIntArray("input/inputday1")
+        
+        maxCalories = self.calorieCalculator.sumNBiggestEaters(calories, 1)
+        self.assertEqual(maxCalories, 70698)
+        
+    def test_SumBiggestThreeEatersInput(self):
+        calories = self.fileReader.readLinesToIntArray("input/inputday1")
+        
+        maxCalories = self.calorieCalculator.sumNBiggestEaters(calories, 3)
+        self.assertEqual(maxCalories, 206643)
 
 if __name__ == '__main__':
     unittest.main()
