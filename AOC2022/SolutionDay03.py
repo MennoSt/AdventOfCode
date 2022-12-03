e702897afec92add7b4146469c742624d08e1c2e
MennoSt <mennosteensma@gmail.com>
2022-12-03 11:03:07 +0100

    added part 2

--- AOC2022/SolutionDay03.py
+++ AOC2022/SolutionDay03.py
@@ -16,11 +16,35 @@ def determineCommonType(input):
     commonCharacters = []
     for compartment in array:
         commonCharacters.append(''.join(set(compartment[0]).intersection(compartment[1])))
-    
+
     return commonCharacters
 
 def calculateSumCommonCharacters(input):
     commonCharacters = determineCommonType(input)
+    return calculateScore(commonCharacters)
+
+def splitCompartmentsPart2(input):
+    array = []
+    for i in range(0,len(input)):
+        if i % 3 == 0:
+            array.append([input[i],input[i+1], input[i+2]])
+    return array
+
+def determineCommonTypePart2(input):
+    array = splitCompartmentsPart2(input)
+    commonCharacters = []
+    for compartment in array:
+        commonChar = ''.join(set(compartment[0]).intersection(compartment[1]))
+        commonCharacters.append(''.join(set(commonChar).intersection(compartment[2])))
+
+    return commonCharacters
+
+def calculateSumCommonCharactersPart2(input):
+    commonCharacters = determineCommonTypePart2(input)
+    return calculateScore(commonCharacters)
+
+
+def calculateScore(commonCharacters):
     score = 0
     for character in commonCharacters:
         asciiNumber = ord(character)
@@ -34,15 +58,19 @@ def calculateSumCommonCharacters(input):
 
 # %%
 # Solution
-
-
 inputArray = fileReader.readLinesToStringArray("input/inputday3")
-sum = calculateSumCommonCharacters(inputArray)
-print(sum)
+answer1 = calculateSumCommonCharacters(inputArray)
+answer2 = calculateSumCommonCharactersPart2(inputArray)
+printAnswer(3, answer1, answer2)
+assert answer1 == 7917
+assert answer2 == 2585
 
+# %%
+# Example Tests
 inputArray = fileReader.readLinesToStringArray("input_ut/inputday3_ut")
-sum = calculateSumCommonCharacters(inputArray)
-assert sum == 157
-# assert sumNBiggestEaters(inputArray, 3) == 157
+sum1 = calculateSumCommonCharactersPart2(inputArray)
+assert sum1 == 70
+sum2 = calculateSumCommonCharacters(inputArray)
+assert sum2 == 157
 
 # %%
