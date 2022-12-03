2028e9dac67afa047f896d69ad3b43a5249904fc
MennoSt <mennosteensma@gmail.com>
2022-12-03 11:56:31 +0100

    refactored a bit

--- AOC2022/SolutionDay03.py
+++ AOC2022/SolutionDay03.py
@@ -11,38 +11,12 @@ def splitCompartments(input):
         array.append([part1,part2])
     return array
 
-def determineCommonType(input):
-    array = splitCompartments(input)
-    commonCharacters = []
-    for compartment in array:
-        commonCharacters.append(''.join(set(compartment[0]).intersection(compartment[1])))
-
-    return commonCharacters
-
-def calculateSumCommonCharacters(input):
-    commonCharacters = determineCommonType(input)
-    return calculateScore(commonCharacters)
-
-def splitCompartmentsPart2(input):
+def divideInGroupsOfThree(input):
     array = []
     for i in range(0,len(input)):
         if i % 3 == 0:
-            array.append([input[i],input[i+1], input[i+2]])
+            array.append([input[i], input[i+1], input[i+2]])
     return array
-
-def determineCommonTypePart2(input):
-    array = splitCompartmentsPart2(input)
-    commonCharacters = []
-    for compartment in array:
-        commonChar = ''.join(set(compartment[0]).intersection(compartment[1]))
-        commonCharacters.append(''.join(set(commonChar).intersection(compartment[2])))
-
-    return commonCharacters
-
-def calculateSumCommonCharactersPart2(input):
-    commonCharacters = determineCommonTypePart2(input)
-    return calculateScore(commonCharacters)
-
 
 def calculateScore(commonCharacters):
     score = 0
@@ -55,12 +29,34 @@ def calculateScore(commonCharacters):
 
     return score
 
+def getCommonCharacters(stringOne, stringTwo):
+    return ''.join(set(stringOne).intersection(stringTwo))
+
+def determineCommonCharacters(array):
+    commonCharacters = []
+    for compartment in array:
+        commonChar = compartment[0]
+        for i in range(1, len(compartment)):
+            commonChar = getCommonCharacters(commonChar, compartment[i])
+        commonCharacters.append(commonChar)
+
+    return commonCharacters
+
+def calculateSumCommonCharacters(input, splitCompartments = False):
+    if splitCompartments == True:
+        array = splitCompartments(input)
+        commonCharacters = determineCommonCharacters(array)
+    else:
+        array = divideInGroupsOfThree(input)
+        commonCharacters = determineCommonCharacters(array)
+    return calculateScore(commonCharacters)
+
 
 # %%
 # Solution
 inputArray = fileReader.readLinesToStringArray("input/inputday3")
-answer1 = calculateSumCommonCharacters(inputArray)
-answer2 = calculateSumCommonCharactersPart2(inputArray)
+answer1 = calculateSumCommonCharacters(inputArray, True)
+answer2 = calculateSumCommonCharacters(inputArray)
 printAnswer(3, answer1, answer2)
 assert answer1 == 7917
 assert answer2 == 2585
@@ -68,9 +64,9 @@ assert answer2 == 2585
 # %%
 # Example Tests
 inputArray = fileReader.readLinesToStringArray("input_ut/inputday3_ut")
-sum1 = calculateSumCommonCharactersPart2(inputArray)
-assert sum1 == 70
+sum1 = calculateSumCommonCharacters(inputArray, True)
+assert sum1 == 157
 sum2 = calculateSumCommonCharacters(inputArray)
-assert sum2 == 157
+assert sum2 == 70
 
 # %%
