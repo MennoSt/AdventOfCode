be9dc7cbc16a7a8e6154dcd0b9f102e4f0feeeee
Menno Steensma <menno.steensma@heliox-energy.com>
2022-12-08 17:17:57 +0100

    refactored day 8 a bit

--- 2022/SolutionDay08.py
+++ 2022/SolutionDay08.py
@@ -15,96 +15,65 @@ def isVisible(grid, x, y):
             return True
         if nextVal >= gridValue:
             break
-        
     for i in range(0, grid.mapWidth):
         nextVal = grid.getValue(x-i, y, Direction.LEFT)
         if nextVal == None:
             return True
         if nextVal >= gridValue:
             break
-        
     for j in range(0, grid.mapHeight):
         nextVal = grid.getValue(x, y-j, Direction.DOWN)
         if nextVal == None:
             return True
         if nextVal >= gridValue:
             break
-        
     for j in range(0, grid.mapHeight):
         nextVal = grid.getValue(x, y+j, Direction.UP)
         if nextVal == None:
             return True
         if nextVal >= gridValue:
             break
-            
+        
     return False
 
 def sceneticScore(grid, x, y):
-    value = 0
     gridValue = grid.getValue(x, y) 
-    visible = False
-    sceneticScore1 = 0
-    sceneticScore2 = 0
-    sceneticScore3 = 0
-    sceneticScore4 = 0
+    scoreArray = [0,0,0,0]
 
-    i = 1
-    value = grid.getValue(x, y-i)
-    while value != None:
-        value = grid.getValue(x, y-i)
-        if value == None:
+    for i in range(0, grid.mapHeight):
+        nextVal = grid.getValue(x+i, y, Direction.RIGHT)
+        if nextVal == None:
             break
-        if value >= gridValue:
-            sceneticScore1 += 1
+        if nextVal >= gridValue:
+            scoreArray[0] += 1
             break
-        i += 1
-        sceneticScore1 += 1
+        scoreArray[0] += 1
+    for i in range(0, grid.mapHeight):
+        nextVal = grid.getValue(x-i, y, Direction.LEFT)
+        if nextVal == None:
+            break
+        if nextVal >= gridValue:
+            scoreArray[1] += 1
+            break
+        scoreArray[1] += 1
+    for i in range(0, grid.mapHeight):
+        nextVal = grid.getValue(x, y-i, Direction.DOWN)
+        if nextVal == None:
+            break
+        if nextVal >= gridValue:
+            scoreArray[2] += 1
+            break
+        scoreArray[2] += 1
+    for i in range(0, grid.mapHeight):
+        nextVal = grid.getValue(x, y+i, Direction.UP)
+        if nextVal == None:
+            break
+        if nextVal >= gridValue:
+            scoreArray[3] += 1
+            break
+        scoreArray[3] += 1
     
-    i = -1
-    value = grid.getValue(x, y-i)    
-    while value != None:
-        value = grid.getValue(x, y-i)
-        if value == None:
-            visible = True
-            break
-        if value >= gridValue:
-            sceneticScore2 += 1
-            visible = False
-            break
-        i -= 1
-        sceneticScore2 += 1
-            
-
-    j = 1
-    value = grid.getValue(x-j, y)
-    while value != None:
-        value = grid.getValue(x-j, y)
-        if value == None:
-            visible = True
-            break
-        if value >= gridValue:
-            sceneticScore3 += 1
-            visible = False
-            break
-        j += 1
-        sceneticScore3 += 1
-        
-    j = -1
-    value = grid.getValue(x-j, y)
-    while value != None:
-        value = grid.getValue(x-j, y)
-        if value == None:
-            visible = True
-            break
-        if value >= gridValue:
-            sceneticScore4 += 1
-            visible = False
-            break
-        j -= 1
-        sceneticScore4 += 1
-    
-    score = sceneticScore1*sceneticScore2*sceneticScore3*sceneticScore4
-    return score
+    return scoreArray[0]*scoreArray[1]*scoreArray[2]*scoreArray[3]
 
 def calcVisibleTrees(input):
     grid = Grid()
@@ -114,44 +83,25 @@ def calcVisibleTrees(input):
         for j in range(0, grid.mapHeight):
             if isVisible(grid,i,j):
                 count+=1
-        
     return count
 
 def calcSceneticScore(input):
     grid = Grid()
     grid.setGrid(input)
     maxScore = 0
-    sceneticScoreCount = 0
     for i in range(0, grid.mapWidth):
         for j in range(0, grid.mapHeight):
-            sceneticScoreCount = sceneticScore(grid, i, j)
-            if sceneticScoreCount > maxScore:
-                maxScore = sceneticScoreCount
+            maxScore = max(maxScore, sceneticScore(grid, i, j))
     return maxScore
 
 # %%
 # Example Tests
 inputStr = fileReader.readToIntMap("input_ut/inpututday8")
-grid = Grid()
-grid.setGrid(inputStr)
-
-# assert isVisible(grid,0,0,) == True
-# assert isVisible(grid,4,0) == True
-# assert isVisible(grid,0,4) == True
-# assert isVisible(grid,4,4) == True
-# assert isVisible(grid,2,2) == False
-# assert isVisible(grid,3,3) == False
-# assert isVisible(grid,2,1) == True
 assert calcVisibleTrees(inputStr) == 21
-assert sceneticScore(grid,4,0) == 0
-assert sceneticScore(grid,0,0) == 0
-assert sceneticScore(grid,2,3) == 8
-calcSceneticScore(inputStr) == 8
+assert calcSceneticScore(inputStr) == 8
 
 # %%
 # Solution
 inputStr = fileReader.readToIntMap("input/inputday8")
-grid = Grid()
-grid.setGrid(inputStr)
 assert calcVisibleTrees(inputStr) == 1776
 assert calcSceneticScore(inputStr) == 234416
