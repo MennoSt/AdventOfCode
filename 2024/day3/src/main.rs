bb531991c85d0a358d0798de8e1d7d4662e37c14
MennoSt <mennosteensma@gmail.com>
2024-12-03 18:54:00 +0100

    small refactor

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -2,121 +2,74 @@ use lib::filereader;
 
 fn main()
 {
-    // let content = filereader::_input("../input/day3");
-    // let part1 = caculate_multiplication_sum(&content);
-    // println!("{}",part1);
-
     let content = filereader::_input("../input/day3");
-    let part2 = caculate_multiplication_sum_part2(&content);
-    println!("{}",part2);
+    let part1 = part1(&content);
+    let part2 = part2(&content);
     
-    // let part1 = total_distance(&int_vectors);
-    // let part2 = total_similarity_score(&int_vectors);
-    
-    // assert_eq!(part1, 1666427);
-    // assert_eq!(part2, 24316233);
-    // println!("{} {}", part1, part2);
+    assert_eq!(part1, 164730528);
+    assert_eq!(part2, 70478672);
+    println!("{} {}", part1, part2);
 }
 
-fn parse_data(input: &str) {
-    let contents = filereader::_input(input);
-}
-
-fn caculate_multiplication_sum(contents: &str) -> i32 {
-    let mut first_digit = String::from("");
-    let mut second_digit = String::from("");
-    let mut start_second_digit = false;
+fn part1(contents: &str) -> i32 {
     let mut multiplication_sum = 0;
-
     for i in 0..contents.len() {
         if starts_with_mulc(contents, i)
             {
-                if let Some(ch) = contents.chars().nth(i+4){
-                    if ch.is_ascii_digit() {
-                        first_digit.push(ch);
-                    }
-                }
-                
-                let mut j = 5;
-                let mut ch = contents.chars().nth(i+j).unwrap();
-                while ch != ')' {
-                    ch = contents.chars().nth(i+j).unwrap();
-                    if ch.is_ascii_digit() && !start_second_digit{
-                        first_digit.push(ch);
-                    } else if ch == ',' {
-                        start_second_digit=true;
-                    } else if ch.is_ascii_digit() && start_second_digit {
-                        second_digit.push(ch);
-                    } else if ch != ')' && start_second_digit {
-                        second_digit.clear();
-                        first_digit.clear();
-                        start_second_digit = false;
-                        break;
-                    } else {
-                        break;
-                    }
-                    j+=1;
-                }
-                
-
-                multiplication_sum += first_digit.parse().unwrap_or(0) * 
-                    second_digit.parse().unwrap_or(0);
-                // println!("{}{}",first_digit, second_digit);
-                first_digit.clear();
-                second_digit.clear();
-                start_second_digit = false;
+                update_sum(contents, i, &mut multiplication_sum);
             }
     }
     return multiplication_sum;
 }
 
-fn caculate_multiplication_sum_part2(contents: &str) -> i32 {
-    let mut first_digit = String::from("");
-    let mut second_digit = String::from("");
-    let mut start_second_digit = false;
-    let mut multiplication_sum = 0;
+fn part2(contents: &str) -> i32 {
     let mut enabled = true;
-
+    let mut multiplication_sum = 0;
+    
     for i in 0..contents.len() {
         toggle_enabled(contents, i, &mut enabled);
         if starts_with_mulc(contents, i) && enabled
-            {
-                if let Some(ch) = contents.chars().nth(i+4){
-                    if ch.is_ascii_digit() {
-                        first_digit.push(ch);
-                    }
-                }
-                
-                let mut j = 5;
-                let mut ch = contents.chars().nth(i+j).unwrap();
-                while ch != ')' {
-                    ch = contents.chars().nth(i+j).unwrap();
-                    if ch.is_ascii_digit() && !start_second_digit{
-                        first_digit.push(ch);
-                    } else if ch == ',' {
-                        start_second_digit=true;
-                    } else if ch.is_ascii_digit() && start_second_digit {
-                        second_digit.push(ch);
-                    } else if ch != ')' && start_second_digit {
-                        second_digit.clear();
-                        first_digit.clear();
-                        start_second_digit = false;
-                        break;
-                    } else {
-                        break;
-                    }
-                    j+=1;
-                }
-
-                multiplication_sum += first_digit.parse().unwrap_or(0) * 
-                    second_digit.parse().unwrap_or(0);
-                // println!("{}{}",first_digit, second_digit);
-                first_digit.clear();
-                second_digit.clear();
-                start_second_digit = false;
-            }
+        {
+            update_sum(contents, i, &mut multiplication_sum);
+        }
     }
     return multiplication_sum;
+}
+
+fn update_sum(contents: &str, i: usize, multiplication_sum: &mut i32) {
+    let mut first_digit = String::from("");
+    let mut second_digit = String::from("");
+    let mut start_second_digit = false;
+    if let Some(ch) = contents.chars().nth(i+4){
+        if ch.is_ascii_digit() {
+            first_digit.push(ch);
+        }
+    }
+                
+    let mut j = 5;
+    let mut ch = contents.chars().nth(i+j).unwrap();
+    while ch != ')' {
+        ch = contents.chars().nth(i+j).unwrap();
+        if ch.is_ascii_digit() && !start_second_digit{
+            first_digit.push(ch);
+        } else if ch == ',' {
+            start_second_digit=true;
+        } else if ch.is_ascii_digit() && start_second_digit {
+            second_digit.push(ch);
+        } else if ch != ')' && start_second_digit {
+            second_digit.clear();
+            first_digit.clear();
+            break;
+        } else {
+            break;
+        }
+        j+=1;
+    }
+
+    *multiplication_sum += first_digit.parse().unwrap_or(0) * 
+        second_digit.parse().unwrap_or(0);
+    first_digit.clear();
+    second_digit.clear();
 }
 
 fn starts_with_mulc(contents: &str, i: usize) -> bool {
@@ -156,41 +109,41 @@ mod tests {
     #[test]
     fn test1() {
         let string = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-        let sum = caculate_multiplication_sum(string);
+        let sum = part1(string);
         assert_eq!(sum, 161);
     }
 
     #[test]
     fn test2() {
         let string = "+mul(32,64]then(mul(11,8)mul(8,5))";
-        let sum = caculate_multiplication_sum(string);
+        let sum = part1(string);
         assert_eq!(sum, 128);
     }
 
     #[test]
     fn test3() {
         let string = "+mul(32,)64]then(mul(11,8)mul(8,5))";
-        let sum = caculate_multiplication_sum(string);
+        let sum = part1(string);
         assert_eq!(sum, 128);
     }
 
     #[test]
     fn test4() {
         let string = "xmul(2,4)";
-        let sum = caculate_multiplication_sum_part2(string);
+        let sum = part2(string);
         assert_eq!(sum, 8);
     }
     #[test]
     fn test5() {
         let string = "don't()xmul(2,4)";
-        let sum = caculate_multiplication_sum_part2(string);
+        let sum = part2(string);
         assert_eq!(sum, 0);
     }
 
     #[test]
     fn test6() {
         let string = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
-        let sum = caculate_multiplication_sum_part2(string);
+        let sum = part2(string);
         assert_eq!(sum, 48);
     }
 }
