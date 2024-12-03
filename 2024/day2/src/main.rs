fb9c0edc020d9ccd34e4b840f5d7f0ce35f38812
MennoSt <mennosteensma@gmail.com>
2024-12-03 07:06:01 +0100

    refactor

--- 2024/day2/src/main.rs
+++ 2024/day2/src/main.rs
@@ -2,21 +2,22 @@ use lib::filereader;
 
 fn main()
 {
-    let part1 = part1();
+    let part1 = calculate_safe_reports(false);
+    let part2 = calculate_safe_reports(true);
+    println!("{}",part1);
+    println!("{}",part2);
     assert_eq!(part1, 279);
+    assert_eq!(part2, 343);
 }
 
-fn part1() -> i32 {
+fn calculate_safe_reports(tolerate_single_bad:bool) -> i32 {
     let vec_numbers = parse_data("../input/day2");
-
     let mut counter = 0;
-    for vec in vec_numbers {
-        if is_safe(vec) {
+    for vec in &vec_numbers {
+        if is_safe(&vec, tolerate_single_bad) {
             counter+=1;
         }
     }
-
-    println!("{:?}",counter);
     counter
 }
 
@@ -34,38 +35,63 @@ fn parse_data(input: &str) -> Vec<Vec<i32>>  {
     vec1
 }
 
-fn is_only_increase_or_decrease(input:&Vec<i32>) -> bool{
+fn is_only_increase_or_decrease(input:&Vec<i32>, tolerate_single_bad:bool) -> bool{
     
     let mut increased = false;
     let mut decreased = false;
     for i  in 1..input.len() {
-        if input[i-1] >= input[i] {
+        if input[i-1] > input[i] {
             decreased = true;
         }
-        if input[i-1] <= input[i] {
+        if input[i-1] < input[i] {
             increased = true;
         }
     }
 
-    if increased && decreased {
-        return false;
+    if ((increased && decreased) || (!increased && !decreased)) && tolerate_single_bad {
+        for i  in 1..input.len() {
+            let mut vec_copy1 = input.clone();
+            let mut vec_copy2 = input.clone();
+            vec_copy1.remove(i);
+            vec_copy2.remove(i-1);
+            if is_only_increase_or_decrease(&vec_copy1, false) && has_correct_difference(&vec_copy1, false)|| 
+            is_only_increase_or_decrease(&vec_copy2, false) && has_correct_difference(&vec_copy2, false) {
+                return true;
+            }
+        }
+    }
+    
+    if (increased && decreased) || (!increased && !decreased) {
+        return false
     }
     true
 }
 
-fn has_correct_difference(input:&Vec<i32>) -> bool {
+fn has_correct_difference(input:&Vec<i32>, tolerate_single_bad:bool) -> bool {
 
     for i  in 1..input.len() {
         let diff = (input[i-1]-input[i]).abs();
-        if diff > 3 || diff == 0 {
+        if diff > 3 || diff < 1{
+            if tolerate_single_bad {
+                let mut vec_copy1 = input.clone();
+                let mut vec_copy2 = input.clone();
+                vec_copy1.remove(i);
+                vec_copy2.remove(i-1);
+                
+                if has_correct_difference(&vec_copy1, false) && is_only_increase_or_decrease(&vec_copy1, false)  || 
+                has_correct_difference(&vec_copy2, false) && is_only_increase_or_decrease(&vec_copy2, false) {
+                    return true;
+                }
+            }
             return false;
         }        
     }
     true
 }
 
-fn is_safe(input:Vec<i32>) -> bool {
-    return is_only_increase_or_decrease(&input) && has_correct_difference(&input)
+fn is_safe(input:&Vec<i32>, tolerate_single_bad:bool) -> bool {
+    return is_only_increase_or_decrease(&input, tolerate_single_bad) && 
+        has_correct_difference(&input,tolerate_single_bad)
 }
 
 #[cfg(test)]
@@ -75,23 +101,42 @@ mod tests {
     #[test]
     fn test1() {
         let numbers = vec![7,6,4,2,1];
-        let is_safe=is_safe(numbers);
+        let is_safe=is_safe(&numbers,true);
         assert_eq!(is_safe, true);
     }
 
     #[test]
     fn test2() {
         let numbers = vec![1,2,7,8,9];
-        let is_safe=is_safe(numbers);
+        let is_safe=is_safe(&numbers,true);
         assert_eq!(is_safe, false);
     }
 
     #[test]
     fn test3() {
         let numbers = vec![9,7,6,2,1];
-        let is_safe=is_safe(numbers);
+        let is_safe=is_safe(&numbers, true);
         assert_eq!(is_safe, false);
     }
 
+    #[test]
+    fn test4() {
+        let numbers = vec![1,3,2,4,5];
+        let is_safe=is_safe(&numbers, true);
+        assert_eq!(is_safe, true);
+    }
 
+    #[test]
+    fn test5() {
+        let numbers = vec![8,6,4,4,1];
+        let is_safe=is_safe(&numbers,true);
+        assert_eq!(is_safe, true);
+    }
+
+    #[test]
+    fn test6() {
+        let numbers = vec![1,3,6,7,9];
+        let is_safe=is_safe(&numbers,true);
+        assert_eq!(is_safe, true);
+    }
 }
