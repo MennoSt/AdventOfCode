b3bda44c51d75135a1c5da86eb4ab2d88e399ef1
MennoSt <mennosteensma@gmail.com>
2024-12-06 16:22:13 +0100

    example

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -21,63 +21,80 @@ enum Direction {
 fn main()
 {
     let grid= parse_data("../input/day06");
-    let part1 = iterate_part1(grid);
+    let part1 = iterate_part1(grid.clone());
     println!("{}",part1);
     assert_eq!(part1, 4454);
+
+    // let part2 = iterate_part2(grid);
+    // println!("{}",part2);
     // assert_eq!(part2, 1933);
 }
 
 
 fn iterate_part2(grid: Grid) -> usize{
   
+    let mut number_of_obstructions = 0;
+
     let mut direction = Direction::Up;
     let start_pos = start_pos(&grid);
     let mut current_pos = start_pos.clone();
-    let mut vec_coord:Vec<Coordinate> = Vec::new();
+    let mut vec_coord:Vec<(Coordinate,Direction)> = Vec::new();
+    vec_coord.push((start_pos.clone(), Direction::Up));
+
+    let mut grid_mut = grid.clone();
     
-    vec_coord.push(start_pos.clone());
-
-    while (current_pos.x < (grid._width()-1) as i32)&& current_pos.x > 0 && 
-        (current_pos.y < (grid._height()-1) as i32) && current_pos.y > 0 
-        {
-        let next_elem;
-        if direction == Direction::Up {
-            next_elem = grid._elem(current_pos.x, current_pos.y-1);
-        } else if direction == Direction::Right {
-            next_elem = grid._elem(current_pos.x+1, current_pos.y);
-        } else if direction == Direction::Down {
-            next_elem = grid._elem(current_pos.x, current_pos.y+1);
-        } else {
-            next_elem = grid._elem(current_pos.x-1, current_pos.y);
-        }
-
-        if next_elem == "#" {
-            if direction == Direction::Up {
-                direction = Direction::Right;
-            } else if direction == Direction::Right {
-                direction = Direction::Down;
-            } else if direction == Direction::Down {
-                direction = Direction::Left;
-            } else {
-                direction = Direction::Up;
+    let mut it =0;
+    for i in 0..grid_mut._width() {
+        for j in 0..grid_mut._height(){
+            grid_mut._set_str(i as i32, j as i32, "#".to_string());
+            if is_loop(&mut current_pos, &grid_mut, &mut direction, &mut vec_coord) {
+                number_of_obstructions +=1;
+                println!("{}","foundObstruction")
             }
-        }
-
-        if direction == Direction::Up {
-            current_pos.y = current_pos.y-1;
-        } else if direction == Direction::Right {
-            current_pos.x = current_pos.x+1;
-        } else if direction == Direction::Down {
-            current_pos.y = current_pos.y+1;
-        } else {
-            current_pos.x = current_pos.x-1;
-        }
-
-        if !vec_coord.contains(&current_pos) {
-            vec_coord.push(current_pos.clone());
+            current_pos = start_pos.clone();
+            vec_coord.clear();
+            direction = Direction::Up;
+            grid_mut = grid.clone();
+            it+=1;
+            println!("iteration {}",it);
         }
     }
-    vec_coord.len()
+
+
+    number_of_obstructions
+}
+
+fn is_loop(mut current_pos: &mut Coordinate, grid_mut: &Grid, direction: &mut Direction, vec_coord: &mut Vec<(Coordinate, Direction)>) -> bool {
+    let mut is_loop = false;
+    while (current_pos.x < (grid_mut._width()-1) as i32)&& current_pos.x >= 0 && 
+        (current_pos.y < (grid_mut._height()-1) as i32) && current_pos.y >= 0 
+        {
+        if next_element(&direction, &grid_mut, &current_pos) == "#" {
+            change_direction(direction);
+        }
+        update_current_position(&direction, current_pos);
+        
+        let tuple = (current_pos.clone(),direction.clone());
+        if vec_coord.contains(&tuple) {
+            is_loop = true;
+            break;
+        }
+        vec_coord.push((current_pos.clone(), direction.clone()));
+    }
+
+    is_loop
+}
+
+fn change_direction(direction: &mut Direction) {
+    if *direction == Direction::Up {
+        *direction = Direction::Right;
+    } else if *direction == Direction::Right {
+        *direction = Direction::Down;
+    } else if *direction == Direction::Down {
+        *direction = Direction::Left;
+    } else {
+        *direction = Direction::Up;
+    }
 }
 
 
@@ -93,44 +110,44 @@ fn iterate_part1(grid: Grid) -> usize{
     while (current_pos.x < (grid._width()-1) as i32)&& current_pos.x > 0 && 
         (current_pos.y < (grid._height()-1) as i32) && current_pos.y > 0 
         {
-        let next_elem;
-        if direction == Direction::Up {
-            next_elem = grid._elem(current_pos.x, current_pos.y-1);
-        } else if direction == Direction::Right {
-            next_elem = grid._elem(current_pos.x+1, current_pos.y);
-        } else if direction == Direction::Down {
-            next_elem = grid._elem(current_pos.x, current_pos.y+1);
-        } else {
-            next_elem = grid._elem(current_pos.x-1, current_pos.y);
+
+        if next_element(&direction, &grid, &current_pos) == "#" {
+            change_direction(&mut direction);
         }
 
-        if next_elem == "#" {
-            if direction == Direction::Up {
-                direction = Direction::Right;
-            } else if direction == Direction::Right {
-                direction = Direction::Down;
-            } else if direction == Direction::Down {
-                direction = Direction::Left;
-            } else {
-                direction = Direction::Up;
-            }
-        }
-
-        if direction == Direction::Up {
-            current_pos.y = current_pos.y-1;
-        } else if direction == Direction::Right {
-            current_pos.x = current_pos.x+1;
-        } else if direction == Direction::Down {
-            current_pos.y = current_pos.y+1;
-        } else {
-            current_pos.x = current_pos.x-1;
-        }
+        update_current_position(&direction, &mut current_pos);
 
         if !vec_coord.contains(&current_pos) {
             vec_coord.push(current_pos.clone());
         }
     }
     vec_coord.len()
+}
+
+fn update_current_position(direction: &Direction, current_pos: &mut Coordinate) {
+    if *direction == Direction::Up {
+        current_pos.y = current_pos.y-1;
+    } else if *direction == Direction::Right {
+        current_pos.x = current_pos.x+1;
+    } else if *direction == Direction::Down {
+        current_pos.y = current_pos.y+1;
+    } else {
+        current_pos.x = current_pos.x-1;
+    }
+}
+
+fn next_element(direction: &Direction, grid: &Grid, current_pos: &Coordinate) -> String {
+    let next_elem;
+    if *direction == Direction::Up {
+        next_elem = grid._elem(current_pos.x, current_pos.y-1);
+    } else if *direction == Direction::Right {
+        next_elem = grid._elem(current_pos.x+1, current_pos.y);
+    } else if *direction == Direction::Down {
+        next_elem = grid._elem(current_pos.x, current_pos.y+1);
+    } else {
+        next_elem = grid._elem(current_pos.x-1, current_pos.y);
+    }
+    next_elem
 }
 
 fn start_pos(grid: &Grid) -> Coordinate {
@@ -170,10 +187,37 @@ fn read_into_grid(contents: &str) -> Grid {
 mod tests {
     use super::*;
 
+    fn isloop_test(str:&str) {
+        let grid= parse_data(str);
+        let start_pos = start_pos(&grid);
+        let mut direction = Direction::Up;
+        let mut current_pos = start_pos.clone();
+        let mut vec_coord:Vec<(Coordinate,Direction)> = Vec::new();
+        vec_coord.push((start_pos.clone(), Direction::Up));
+    
+        let answer = is_loop(&mut current_pos, &grid, &mut direction, &mut vec_coord);
+        assert_eq!(answer,true);
+    }
+
     #[test]
     fn test1() {
         let grid= parse_data("test1");
         let answer = iterate_part1(grid);
         assert_eq!(answer,41);
+    }
+
+    #[test]
+    fn test2() {
+        isloop_test("test2");
+        isloop_test("test3");
+        isloop_test("test3");
+    }
+
+    
+    #[test]
+    fn test3() {
+        let grid= parse_data("test1");
+        let answer = iterate_part2(grid);
+        assert_eq!(answer,6);
     }
 }
