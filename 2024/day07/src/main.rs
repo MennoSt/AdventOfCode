5f2377ec8dbdf37939c33c3d55c295610d391ac4
MennoSt <mennosteensma@gmail.com>
2024-12-07 18:42:01 +0100

    part2 day 7

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -1,13 +1,24 @@
-use itertools::Position;
-use lib::filereader;
-use lib::grid::Grid;
-use lib::utils::*;
+use std::vec;
 
-struct Calculation {
-    answer:i128,
-    values:Vec<i128>,
+use lib::filereader;
+
+fn remove_last_digit_if_matches(mut num: i128, x: i128) -> (i128, bool) {
+    let mut match_found = false;
+    let value = x.to_string();
+    let n = value.len();
+    let divisor = 10_i32.pow(n as u32) as i128; // Calculate 10^n to isolate the last `n` digits
+    if num % divisor == x {
+        num /= divisor; // Remove the last `n` digits
+        match_found=true;
+    }
+    (num, match_found)
 }
 
+
+// 13058408260908
+// 13058408731545
+// 162059698466155
+// 162042343638683
 fn main()
 {
     let part1 = part1("../input/day07");
@@ -15,10 +26,27 @@ fn main()
     println!("{}",part1);
     assert_eq!(part1, 1260333054159);
     
+    let part2 = part2("../input/day07");
+    println!("{}",part2);
+    assert_eq!(part2,162042343638683);
+    
     // let part2 = part2(grid);
     // println!("{}",part2);
     // assert_eq!(part2, 1503);
 }
+fn part2(input:&str) -> i128{
+    let calculations = parse_data(input);
+    let mut sum = 0;
+    for calculation in &calculations {
+        if calculate_configurations_part2(calculation) > 0 {
+            sum += calculation.0;
+        } else {
+            println!("{:?}",calculation);
+        }
+    }
+    sum
+}
+
 fn part1(input:&str) -> i128{
     let calculations = parse_data(input);
     let mut sum = 0;
@@ -28,6 +56,44 @@ fn part1(input:&str) -> i128{
         }
     }
     sum
+}
+
+fn calculate_configurations_part2(calculation:&(i128,Vec<i128>)) ->i128 {
+    let mut values = calculation.1.clone();
+    values.reverse();
+    let answer = calculation.0;
+    let mut configurations = 0;
+
+    let mut it_vec = vec![answer];
+    let mut i = 0;
+    while i < (values.len()-1){
+        let mut it_vec_copy = Vec::new();
+        for it in &it_vec {
+            if it%values[i] == 0 {
+                let it1 = it / values[i];
+                it_vec_copy.push(it1);
+            }
+            
+            let it2 = it - values[i];
+            it_vec_copy.push(it2);
+            
+            let it3 = remove_last_digit_if_matches(it.clone(), values[i]);
+            if it3.1 {
+                it_vec_copy.push(it3.0);
+            }
+        }
+        it_vec = it_vec_copy;
+        i += 1;
+    }
+
+    let vec_size = values.len();
+    for value in it_vec {
+        if value == values[vec_size-1] {
+            configurations += 1;
+        }
+    }
+
+    configurations
 }
 
 fn calculate_configurations(calculation:&(i128,Vec<i128>)) ->i128 {
@@ -103,9 +169,49 @@ mod tests {
         assert_eq!(answer,1);
     }
     
+    
     #[test]
     fn test4() {
         let answer = part1("test1");
         assert_eq!(answer, 3749);
+    }
+    
+    #[test]
+    fn test5() {
+        let calculation = (156, vec![15,6]);
+        let answer = calculate_configurations_part2(&calculation);
+        assert_eq!(answer,1);
+    }
+
+    #[test]
+    fn test6() {
+        let calculation = (7290, vec![6,8,6,15]);
+        let answer = calculate_configurations_part2(&calculation);
+        assert_eq!(answer,1);
+    }
+
+    #[test]
+    fn test7() {
+        let answer = part2("test1");
+        assert_eq!(answer, 11387);
+    }
+    #[test]
+    fn test8() {
+        let calculation = (1491, vec![1,491]);
+        let answer = calculate_configurations_part2(&calculation);
+        assert_eq!(answer,1);
+    }
+    
+    #[test]
+    fn test9() {
+        let answer = remove_last_digit_if_matches(1491,491);
+        assert_eq!(answer.0,1);
+    }
+
+    #[test]
+    fn test10() {
+        let answer = remove_last_digit_if_matches(12,4);
+        assert_eq!(answer.0,12);
+    
     }
 }
