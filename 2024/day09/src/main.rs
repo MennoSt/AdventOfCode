6c9d0d8cc06348a5d825fd5fe3047efddcb46114
MennoSt <mennosteensma@gmail.com>
2024-12-09 22:17:10 +0100

    part1 solved

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -21,14 +21,82 @@ struct FileBlock {
 fn main() {
     let start = Instant::now();
 
-    let content = filereader::_input(TESTINPUT);
-
-    let (filled, empty) = parse_data(content);
-    println!("{:?}",filled);
-    println!("{:?}",empty);
+    let part1 = calculate_checksum(INPUT);
+    println!("{}",part1);
+    assert_eq!(part1,6398252054886);
 
     let duration = start.elapsed();
     println!("Execution time: {:?}", duration);
+}
+
+fn move_file_blocks_p2(disk_map:(Vec<i32>, Vec<i32>)) -> Vec<FileBlock> {
+    let filled = disk_map.0.clone();
+    let mut empty = disk_map.1.clone();
+    let mut fileblock = Vec::new();
+
+    for i in 0..filled.len() {
+        fileblock.push(FileBlock {value:i as i32, count:filled[i]});
+    }
+
+    let mut i_em = 0;
+    let mut insert_index = 1;
+    while i_em < (empty.len() -1) && insert_index < (fileblock.len()-1) {
+        let fb_index = fileblock.len() -1;
+        if fileblock[fb_index].count < empty[i_em] {
+            fileblock.insert(insert_index,FileBlock{value:fileblock[fb_index].value, count:fileblock[fb_index].count});
+            empty[i_em] -= fileblock[fb_index+1].count;
+            fileblock.pop();
+            insert_index+=1
+        } else if  fileblock[fb_index].count == empty[i_em] {
+            fileblock.insert(insert_index,FileBlock{value:fileblock[fb_index].value, count:empty[i_em]});
+            fileblock.pop();
+            i_em += 1;
+            insert_index+=2;
+        }
+        i_em+=1;
+    }
+
+    let mut index = 0;
+    while index < fileblock.len(){
+        if fileblock[index].count == 0 {
+            fileblock.remove(index);
+        }
+        index+=1;
+    }
+
+    // let fileblock = FileBlock {value:0,count:0};
+    fileblock
+}
+
+fn calculate_checksum_p2(input:&str) -> i128 {
+    let content = filereader::_input(input);
+    let diskmap = parse_data(content);
+    let fileblocks = move_file_blocks_p2(diskmap);
+
+    let mut position:i128 = 0;
+    let mut checksum:i128 = 0;
+    for block in fileblocks {
+        for _ in 0..block.count {
+            checksum += block.value as i128 * position as i128;
+            position += 1;
+        }
+    }
+    checksum
+}
+fn calculate_checksum(input:&str) -> i128 {
+    let content = filereader::_input(input);
+    let diskmap = parse_data(content);
+    let fileblocks = move_file_blocks(diskmap);
+
+    let mut position:i128 = 0;
+    let mut checksum:i128 = 0;
+    for block in fileblocks {
+        for _ in 0..block.count {
+            checksum += block.value as i128 * position as i128;
+            position += 1;
+        }
+    }
+    checksum
 }
 
 fn move_file_blocks(disk_map:(Vec<i32>, Vec<i32>)) -> Vec<FileBlock> {
@@ -42,15 +110,16 @@ fn move_file_blocks(disk_map:(Vec<i32>, Vec<i32>)) -> Vec<FileBlock> {
 
     let mut i_em = 0;
     let mut insert_index = 1;
-    while i_em < empty.len() {
+    
+    while i_em < (empty.len() -1) && insert_index < (fileblock.len()-1) {
         
         let fb_index = fileblock.len() -1;
         if fileblock[fb_index].count < empty[i_em] {
             fileblock.insert(insert_index,FileBlock{value:fileblock[fb_index].value, count:fileblock[fb_index].count});
+            empty[i_em] -= fileblock[fb_index+1].count;
             fileblock.pop();
-            empty[i_em] -= fileblock[fb_index].count;
             insert_index+=1
-        } else if  fileblock[fb_index].count == empty[i_em]{
+        } else if  fileblock[fb_index].count == empty[i_em] {
             fileblock.insert(insert_index,FileBlock{value:fileblock[fb_index].value, count:empty[i_em]});
             fileblock.pop();
             i_em += 1;
@@ -61,13 +130,14 @@ fn move_file_blocks(disk_map:(Vec<i32>, Vec<i32>)) -> Vec<FileBlock> {
             i_em += 1;
             insert_index+=2;
         }
-
-
     }
-    for i in 0..fileblock.len() {
-        if fileblock[i].count == 0 {
-            fileblock.remove(i);
+
+    let mut index = 0;
+    while index < fileblock.len(){
+        if fileblock[index].count == 0 {
+            fileblock.remove(index);
         }
+        index+=1;
     }
 
     // let fileblock = FileBlock {value:0,count:0};
@@ -106,5 +176,17 @@ mod tests {
                                                       FileBlock{value:1,count:3}, FileBlock{value:2,count:3}];
         
         assert_eq!(fileblock, expected_fileblock);
+    }
+    
+    #[test]
+    fn test2() {
+        let checksum = calculate_checksum(TESTINPUT); 
+        assert_eq!(checksum, 1928);
+    }
+    
+    #[test]
+    fn test3() {
+        let checksum = calculate_checksum_p2(TESTINPUT); 
+        assert_eq!(checksum, 2858);
     }
 }
