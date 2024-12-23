e70a97d7aab7a52fc267140f45208b10938d6a40
MennoSt <mennosteensma@gmail.com>
2024-12-23 13:20:59 +0100

    new test fixed

--- 2024/day12_part2/src/main.rs
+++ 2024/day12_part2/src/main.rs
@@ -37,42 +37,56 @@ fn part2(file:&str) -> i32
             let mut first_iter = true;
             let mut plant = Plant{fence_type:fence_type, count:0, fences:0};
             if visited_grid._elem(x,y) != 1 {
-                total_price += calculate_fences(x, y, &grid, &mut visited_grid, &mut plant, &mut first_iter)
+                total_price += calculate_fences(x, y, &grid, &mut visited_grid, &mut plant);
             }
         }
     }
     total_price
 }
 
-fn calculate_fences(x:i32, y:i32, grid:&Grid, visited_grid: &mut Gridi32, plant:&mut Plant, first_it: &mut bool) -> i32
+fn calculate_fences(x:i32, y:i32, grid:&Grid, visited_grid: &mut Gridi32, plant:&mut Plant) -> i32
 {
     visited_grid._set(x, y,1);
     let directions = vec![Coordinate{x:x-1,y:y}, 
-                                            Coordinate{x:x+1,y:y}, 
                                             Coordinate{x:x,y:y-1}, 
+                                            Coordinate{x:x+1,y:y}, 
                                             Coordinate{x:x,y:y+1}];
 
     
-    if is_straight(x, y, grid, plant) && !*first_it {
-        plant.fences -= 2;
+    plant.count += 1;
+
+    if is_not_surrounded(x,y,grid, plant) {
+        plant.fences += 4;
+    }
+    
+    if is_straight(x, y, grid, plant) {
+        plant.fences += 0;
     }
 
-    if is_corner(x,y,grid, plant) {
-        plant.fences -= 1;
+    let corner = is_corner(x, y, grid, plant);
+    if corner.0 && corner.1{
+        plant.fences += 2;
+    } else if corner.0 {
+        plant.fences += 1;
     }
-    
-    if *first_it {
-        *first_it = false;
+
+    if is_single_end(x,y,grid,plant) {
+        plant.fences += 2;
     }
+
+    if is_t_shape(x, y, grid, plant) {
+        
+    }
+    // if is_cross(x,y,grid,plant) {
+    //     plant.fences -= 3
+    // }
     
-    plant.fences += 4;
-    plant.count += 1;
     for dir in directions {
         if grid._is_within_grid(dir.x,dir.y) {
             if plant.fence_type ==  grid._elem(dir.x, dir.y) {
-                plant.fences -= 1;
+                // plant.fences -= 1;
                 if visited_grid._elem(dir.x, dir.y) != 1 {
-                    calculate_fences(dir.x, dir.y, grid, visited_grid, plant, first_it);
+                    calculate_fences(dir.x, dir.y, grid, visited_grid, plant);
                 }
             }
         }
@@ -80,20 +94,133 @@ fn calculate_fences(x:i32, y:i32, grid:&Grid, visited_grid: &mut Gridi32, plant:
     plant.fences * plant.count
 }
 
-fn is_corner(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
+fn is_t_shape(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
+    let mut is_t_shape = false;
+    let mut is_free_corner = false;
+    
+    if grid._left(x, y) != plant.fence_type && 
+        grid._right(x,y) == plant.fence_type &&
+        grid._up(x, y) == plant.fence_type &&
+        grid._down(x, y) == plant.fence_type {
+            is_t_shape = true;
+            if grid._right(x, y-1) != plant.fence_type && grid._right(x, y+1) != plant.fence_type {
+                plant.fences += 2;
+            } else if grid._right(x, y-1) != plant.fence_type {
+                plant.fences += 1;
+            } else if grid._right(x, y+1) != plant.fence_type {
+                plant.fences += 1;
+            }
+        }
+    
+    
+    if grid._left(x, y) == plant.fence_type && 
+        grid._right(x,y) != plant.fence_type &&
+        grid._up(x, y) == plant.fence_type &&
+        grid._down(x, y) == plant.fence_type {
+            is_t_shape = true;
+            if grid._left(x, y-1) != plant.fence_type && grid._left(x, y+1) != plant.fence_type {
+                plant.fences += 2;
+            } else if grid._left(x, y-1) != plant.fence_type {
+                plant.fences += 1;
+            } else if grid._left(x, y+1) != plant.fence_type {
+                plant.fences += 1;
+            }
+        }
+    
+    if grid._left(x, y) == plant.fence_type && 
+        grid._right(x,y) == plant.fence_type &&
+        grid._up(x, y) != plant.fence_type &&
+        grid._down(x, y) == plant.fence_type {
+            is_t_shape = true;
+            if grid._right(x, y+1) != plant.fence_type && grid._left(x, y+1) != plant.fence_type {
+                plant.fences += 2;
+            } else if grid._right(x, y+1) != plant.fence_type {
+                plant.fences += 1;
+            } else if grid._left(x, y+1) != plant.fence_type {
+                plant.fences += 1;
+            }
+        }
+    
+    if grid._left(x, y) == plant.fence_type && 
+        grid._right(x,y) == plant.fence_type &&
+        grid._up(x, y) == plant.fence_type &&
+        grid._down(x, y) != plant.fence_type {
+            is_t_shape = true;
+            if grid._right(x, y-1) != plant.fence_type && grid._left(x, y-1) != plant.fence_type {
+                plant.fences += 2;
+            } else if grid._right(x, y-1) != plant.fence_type {
+                plant.fences += 1;
+            } else if grid._left(x, y-1) != plant.fence_type {
+                plant.fences += 1;
+            }
+        }
+
+
+    is_t_shape
+}
+
+fn is_not_surrounded(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
+    grid._left(x, y) != plant.fence_type && 
+    grid._right(x,y) != plant.fence_type &&
+    grid._up(x, y) != plant.fence_type &&
+    grid._down(x,y) != plant.fence_type
+}
+
+fn is_single_end(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
+    let mut is_single_end = false;
+
+    if grid._left(x, y) == plant.fence_type && 
+        grid._right(x,y) != plant.fence_type &&
+        grid._up(x, y) != plant.fence_type &&
+        grid._down(x,y) != plant.fence_type {
+        is_single_end = true;
+    }
+
+    if grid._left(x, y) != plant.fence_type && 
+        grid._right(x,y) == plant.fence_type &&
+        grid._up(x, y) != plant.fence_type &&
+        grid._down(x,y) != plant.fence_type {
+        is_single_end = true;
+    } 
+    
+    if grid._left(x, y) != plant.fence_type && 
+        grid._right(x,y) != plant.fence_type &&
+        grid._up(x, y) == plant.fence_type &&
+        grid._down(x,y) != plant.fence_type {
+        is_single_end = true;
+    }
+
+    if grid._left(x, y) != plant.fence_type && 
+        grid._right(x,y) != plant.fence_type &&
+        grid._up(x, y) != plant.fence_type &&
+        grid._down(x,y) == plant.fence_type {
+        is_single_end = true;
+    }
+    is_single_end
+}
+
+fn is_corner(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> (bool,bool) {
     let mut is_corner = false;
-    if grid._left(x, y) == plant.fence_type && 
-        grid._right(x,y) != plant.fence_type &&
-        grid._up(x, y) != plant.fence_type &&
-        grid._down(x, y) == plant.fence_type {
-            is_corner = true;
-        }
+    let mut is_free_corner = false;
     
     if grid._left(x, y) == plant.fence_type && 
         grid._right(x,y) != plant.fence_type &&
         grid._up(x, y) == plant.fence_type &&
         grid._down(x, y) != plant.fence_type {
             is_corner = true;
+            if grid._left(x, y-1) != plant.fence_type {
+                is_free_corner = true;
+            }
+        }
+
+    if grid._left(x, y) == plant.fence_type && 
+        grid._right(x,y) != plant.fence_type &&
+        grid._up(x, y) != plant.fence_type &&
+        grid._down(x, y) == plant.fence_type {
+            is_corner = true;
+            if grid._left(x, y+1) != plant.fence_type {
+                is_free_corner = true;
+            }
         }
 
     if grid._left(x, y) != plant.fence_type && 
@@ -101,17 +228,22 @@ fn is_corner(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
         grid._up(x, y) == plant.fence_type &&
         grid._down(x, y) != plant.fence_type {
             is_corner = true;
+            if grid._right(x, y-1) != plant.fence_type {
+                is_free_corner = true;
+            }
         }
-
+    
     if grid._left(x, y) != plant.fence_type && 
         grid._right(x,y) == plant.fence_type &&
         grid._up(x, y) != plant.fence_type &&
         grid._down(x, y) == plant.fence_type {
             is_corner = true;
-        
+            if grid._right(x, y+1) != plant.fence_type {
+                is_free_corner = true;
+            }
         }
-    
-    is_corner
+
+        (is_corner, is_free_corner)
 
 }
 
@@ -129,42 +261,6 @@ fn is_straight(x: i32, y: i32, grid: &Grid, plant: &mut Plant) -> bool {
         grid._up(x, y) == plant.fence_type &&
         grid._down(x, y) == plant.fence_type {
             is_straight = true;
-        }
-
-    if grid._left(x, y) == plant.fence_type && 
-        grid._right(x,y) != plant.fence_type &&
-        grid._up(x, y) != plant.fence_type &&
-        grid._down(x, y) != plant.fence_type {
-            if grid._left_left(x,y) == plant.fence_type {
-                is_straight = true;
-            }
-        }
-
-    if grid._left(x, y) != plant.fence_type && 
-        grid._right(x,y) == plant.fence_type &&
-        grid._up(x, y) != plant.fence_type &&
-        grid._down(x, y) != plant.fence_type {
-            if grid._right_right(x,y) == plant.fence_type {
-                is_straight = true;
-            }
-        }
-
-    if grid._left(x, y) != plant.fence_type && 
-        grid._right(x,y) != plant.fence_type &&
-        grid._up(x, y) == plant.fence_type &&
-        grid._down(x, y) != plant.fence_type {
-            if grid._up_up(x,y) == plant.fence_type {
-                is_straight = true;
-            }
-    }
-    
-    if grid._left(x, y) != plant.fence_type && 
-        grid._right(x,y) != plant.fence_type &&
-        grid._up(x, y) != plant.fence_type &&
-        grid._down(x, y) == plant.fence_type {
-            if grid._down_down(x,y) == plant.fence_type {
-                is_straight = true;
-            }
         } 
 
     is_straight
@@ -182,6 +278,12 @@ mod tests {
     }
 
     #[test]
+    fn test2() {
+        let part1 = part2("test2");
+        assert_eq!(part1, 40);
+    }
+
+    #[test]
     fn test3() {
         let part1 = part2("test3");
         assert_eq!(part1, 16);
@@ -196,7 +298,19 @@ mod tests {
     #[test]
     fn test5() {
         let part1 = part2("test5");
-        assert_eq!(part1, 64);
+        assert_eq!(part1, 236);
+    }
+
+    #[test]
+    fn test6() {
+        let part1 = part2("test6");
+        assert_eq!(part1, 368);
+    }
+
+    #[test]
+    fn test7() {
+        let part1 = part2("test7");
+        assert_eq!(part1, 1206);
     }
 
 }
