50956fb9d5f25b5952cfa2d59747af81b05ff0be
MennoSt <mennosteensma@gmail.com>
2024-12-31 18:34:58 +0100

    refac a bit

--- 2024/day15/src/main.rs
+++ 2024/day15/src/main.rs
@@ -3,7 +3,6 @@ use lib::utils;
 use lib::utils::Coordinate;
 use std::time::Instant;
 use lib::grid::Grid;
-use std::time::Duration;
 
 static INPUT: &str = "../input/day15";
 
@@ -45,76 +44,26 @@ fn start_pos(grid:&Grid) -> Coordinate {
     Coordinate{x:0,y:0}
 }
 
-
-fn start_pos_p1(grid:&Grid) -> Coordinate {
-    for i in 0..grid._width() as i32{
-        for j in 0..grid._height() as i32 {
-            if grid._elem(i,j) == "@" {
-                return Coordinate {x:i, y:j};
-            }
-        }
-    }
-    Coordinate{x:0,y:0}
-}
 fn move_robot_p1(robot_pos:&mut Coordinate, grid:&mut Grid, movement:&str) {
     let mut depth = 0;
     let mut it_pos = robot_pos.clone();
 
-    change_pos_p1(movement, &mut it_pos);
+    change_pos(movement, &mut it_pos);
     try_move_p1(robot_pos, &mut it_pos, movement, &mut depth, grid)
 }
 
-fn change_pos_p1(movement: &str, it_pos: &mut Coordinate) {
-    match movement {
-        ">" => it_pos.x += 1,
-        "<" => it_pos.x -= 1,
-        "^" => it_pos.y -= 1,
-        "v" => it_pos.y += 1,
-        _ => println!("Unknown command"),
-    }
-}
 fn try_move_p1(robot_pos:&mut Coordinate, it_pos:&mut Coordinate, movement:&str, depth:&mut i32,  grid:&mut Grid) {
     *depth += 1;
 
     if grid._elem(it_pos.x, it_pos.y) == "." && *depth == 1 {
-        update_robot_pos_p1(robot_pos, movement, grid);
+        update_robot_pos(robot_pos, movement, grid);
     } else if grid._elem(it_pos.x, it_pos.y) == "O" {
-        change_pos_p1(movement, it_pos);
+        change_pos(movement, it_pos);
         try_move_p1(robot_pos, it_pos, movement, depth, grid);
     } else if grid._elem(it_pos.x, it_pos.y) == "." {
-        update_robot_pos_p1(robot_pos, movement, grid);
+        update_robot_pos(robot_pos, movement, grid);
         grid._set_str(it_pos.x, it_pos.y,"O".to_string());
     } 
-}
-
-fn update_robot_pos_p1(robot_pos: &mut Coordinate, movement: &str, grid: &mut Grid) {
-    grid._set_str(robot_pos.x, robot_pos.y,".".to_string());
-    change_pos_p1(movement, robot_pos);
-    grid._set_str(robot_pos.x, robot_pos.y,"@".to_string());
-}
-
-fn part1(input:&str) -> i32 {
-    let (mut grid, moves) = parse_data(input);
-    let mut rp = start_pos_p1(&grid);
-
-    for m in moves {
-        move_robot_p1(&mut rp, &mut grid, m.as_str());
-    }
-
-    sum_gps_boxes_p1(grid)
-}
-
-fn sum_gps_boxes_p1(grid: Grid) -> i32 {
-    let mut sum_gps_boxes = 0;
-
-    for i in 0..grid._width() as i32 {
-        for j in 0..grid._height() as i32 {
-           if grid._elem(i, j) == "O" {
-            sum_gps_boxes += 100 * j + i;
-           }
-        }
-    }
-    sum_gps_boxes
 }
 
 fn move_robot_p2(robot_pos:&mut Coordinate, grid:&mut Grid, movement:&str) {
@@ -128,7 +77,6 @@ fn move_robot_p2(robot_pos:&mut Coordinate, grid:&mut Grid, movement:&str) {
     if movement == ">" || movement == "<" {
         try_move_hor(robot_pos, &mut it_pos, movement, &mut depth, grid, &mut vec_elem);
     } else {
-
         let elems_to_check=vec![Coordinate{x:it_pos.x, y:it_pos.y}];
         is_blocked(&mut it_pos, movement,grid, &elems_to_check, &mut blocked, &mut linked_boxes);
         linked_boxes.sort(); // Sort the vector first
@@ -176,65 +124,62 @@ fn remove_brackets(grid: &mut Grid) {
     }
 }
 
-fn is_blocked(it_pos:&mut Coordinate, movement:&str, grid:&mut Grid, elems_to_check: &Vec<Coordinate>, blocked:&mut bool, linked_boxes:&mut Vec<Coordinate>)
+fn is_blocked(it_pos:&mut Coordinate, movement:&str, grid:&mut Grid, coordinates: &Vec<Coordinate>, blocked:&mut bool, linked_boxes:&mut Vec<Coordinate>)
 {
-    let mut blocked_exception = false;
-    if is_free(grid, elems_to_check) {
+    let mut blocked_by_hash = false;
+    if is_free(grid, coordinates) {
         *blocked = false;
     }
 
-    let mut new_elems_to_check = Vec::new();
+    let mut new_coordinates = Vec::new();
 
     if *blocked {
-        if movement == "^" {
-            for e in elems_to_check{
-                let elem = grid._elem(e.x, e.y);
+        for coor in coordinates{
+            if movement == "^" {
+                let elem = grid._elem(coor.x, coor.y);
                 if elem == "#" {
-                    *blocked = true;
-                    if grid._elem(e.x, e.y+1)!="."{
-                        blocked_exception=true;
+                    if grid._elem(coor.x, coor.y+1)!="."{
+                        blocked_by_hash=true;
                     }
                 }
-                let ynew = e.y - 1;
 
+                let ynew = coor.y - 1;
                 if elem == "[" {
-                    let xnew=e.x+1;
-                    new_elems_to_check.push(Coordinate{x:e.x, y:ynew});
-                    new_elems_to_check.push(Coordinate{x:xnew, y:ynew});
+                    let xnew=coor.x+1;
+                    new_coordinates.push(Coordinate{x:coor.x, y:ynew});
+                    new_coordinates.push(Coordinate{x:xnew, y:ynew});
                 }
                 if elem == "]" {
-                    let xnew=e.x-1;
-                    new_elems_to_check.push(Coordinate{x:e.x, y:ynew});
-                    new_elems_to_check.push(Coordinate{x:xnew, y:ynew});
+                    let xnew=coor.x-1;
+                    new_coordinates.push(Coordinate{x:coor.x, y:ynew});
+                    new_coordinates.push(Coordinate{x:xnew, y:ynew});
                 }
-            }
-        } else if movement=="v"{
-            for e in elems_to_check{
-                let elem = grid._elem(e.x, e.y);
+            } else if movement=="v"{
+                let elem = grid._elem(coor.x, coor.y);
                 if elem == "#" {
-                    *blocked = true;
-                    if grid._elem(e.x, e.y-1)!="."{
-                        blocked_exception=true;
+                    if grid._elem(coor.x, coor.y-1)!="."{
+                        blocked_by_hash=true;
                     }
-                }
-                let ynew = e.y + 1;
+                t}
+
+                let ynew = coor.y + 1;
                 if elem == "[" {
-                    let xnew=e.x+1;
-                    new_elems_to_check.push(Coordinate{x:e.x, y:ynew});
-                    new_elems_to_check.push(Coordinate{x:xnew, y:ynew});
+                    let xnew=coor.x+1;
+                    new_coordinates.push(Coordinate{x:coor.x, y:ynew});
+                    new_coordinates.push(Coordinate{x:xnew, y:ynew});
                 }
                 if elem == "]" {
-                    let xnew=e.x-1;
-                    new_elems_to_check.push(Coordinate{x:e.x, y:ynew});
-                    new_elems_to_check.push(Coordinate{x:xnew, y:ynew});
+                    let xnew=coor.x-1;
+                    new_coordinates.push(Coordinate{x:coor.x, y:ynew});
+                    new_coordinates.push(Coordinate{x:xnew, y:ynew});
                 }
             }
         }
     }
 
-   if new_elems_to_check.len()>0 && !blocked_exception {
-      linked_boxes.extend(new_elems_to_check.clone());
-      is_blocked(it_pos, movement, grid, &new_elems_to_check, blocked, linked_boxes);
+   if new_coordinates.len()>0 && !blocked_by_hash {
+      linked_boxes.extend(new_coordinates.clone());
+      is_blocked(it_pos, movement, grid, &new_coordinates, blocked, linked_boxes);
    }
 }
 
@@ -316,6 +261,30 @@ fn extend_grid (grid:&Grid) -> Grid {
     Grid{grid_vec:grid_vec_extended}
 }
 
+fn sum_gps_boxes(grid: Grid, elem:&str) -> i32 {
+    let mut sum_gps_boxes = 0;
+
+    for i in 0..grid._width() as i32 {
+        for j in 0..grid._height() as i32 {
+           if grid._elem(i, j) == elem {
+            sum_gps_boxes += 100 * j + i;
+           }
+        }
+    }
+    sum_gps_boxes
+}
+
+fn part1(input:&str) -> i32 {
+    let (mut grid, moves) = parse_data(input);
+    let mut rp = start_pos(&grid);
+
+    for m in moves {
+        move_robot_p1(&mut rp, &mut grid, m.as_str());
+    }
+
+    sum_gps_boxes(grid, "O")
+}
+
 fn part2(input:&str) -> i32 {
     let (grid, moves) = parse_data(input);
     let mut grid = extend_grid(&grid);
@@ -325,20 +294,7 @@ fn part2(input:&str) -> i32 {
         move_robot_p2(&mut rp, &mut grid, m.as_str());
     }
 
-    sum_gps_boxes(grid)
-}
-
-fn sum_gps_boxes(grid: Grid) -> i32 {
-    let mut sum_gps_boxes = 0;
-
-    for i in 0..grid._width() as i32 {
-        for j in 0..grid._height() as i32 {
-           if grid._elem(i, j) == "[" {
-            sum_gps_boxes += 100 * j + i;
-           }
-        }
-    }
-    sum_gps_boxes
+    sum_gps_boxes(grid,"[")
 }
 
 fn main() {
@@ -398,7 +354,7 @@ mod tests {
         
         let mut rp = start_pos(&grid);
         move_robot_p2(&mut rp, &mut grid, m);
-        let sum = sum_gps_boxes(grid.clone());
+        let sum = sum_gps_boxes(grid.clone(), "[");
         assert_eq!(sum,511);
     }
 
@@ -409,7 +365,7 @@ mod tests {
         
         let mut rp = start_pos(&grid);
         move_robot_p2(&mut rp, &mut grid, m);
-        let sum = sum_gps_boxes(grid.clone());
+        let sum = sum_gps_boxes(grid.clone(), "[");
         grid._print();
         assert_eq!(sum,711);
     }
@@ -422,8 +378,7 @@ mod tests {
         grid._print();
         let mut rp = start_pos(&grid);
         move_robot_p2(&mut rp, &mut grid, m);
-        let sum = sum_gps_boxes(grid.clone());
-        grid._print();
+        let sum = sum_gps_boxes(grid.clone(), "[");
         assert_eq!(sum,1124);
     }
 }
