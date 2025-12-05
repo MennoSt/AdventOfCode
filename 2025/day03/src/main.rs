d629a7da106c7eaac58b5c3320994febd8ab277e
MennoSt <mennosteensma@gmail.com>
2025-12-05 20:19:21 +0100

    p1 day03

---
+++ 2025/day03/src/main.rs
@@ -0,0 +1,120 @@
+use std::vec;
+
+use lib::{filereader, utils};
+
+fn parse_data(input: &str) -> Vec<String> {
+    let contents = filereader::_input(input);
+    let mut data: Vec<String> = Vec::new();
+
+    for c in contents.lines() {
+        data.push(c.to_string());
+    }
+    println!("{:?}", data);
+    data
+}
+
+fn largest_joltage(input: &str, index: &mut i64, joltage: &mut String) {
+    for i in ('1'..='9').rev() {
+        if joltage.len() == 2 {
+            return;
+        }
+        let positions: Vec<usize> = input
+        .char_indices()                  // iterate over (index, char)
+        .filter(|(_, c)| *c == i)     // keep only '9'
+        .map(|(i, _)| i)                 // take the index
+        .collect();
+
+        if let Some(foundindex) = input.find(i) {
+            println!("First {} at index {}", i, foundindex);
+            for p in positions{
+                if p as i64 > *index {
+                    let lastIndexAndSizeZero = p == input.len() - 1 && joltage.len() == 0;
+                    if !lastIndexAndSizeZero {
+                        joltage.push(i);
+                        *index = p as i64;
+                        largest_joltage(input, index, joltage);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn p1(input: &str) -> i64 {
+    let data = parse_data(input);
+    let mut sum = 0;
+
+    for d in data {
+        let mut joltage = "".to_string();
+        largest_joltage(&d, &mut -1, &mut joltage);
+        let j:i64 = joltage.parse().unwrap();
+        sum += j;
+    }
+    sum
+}
+
+fn main() {
+    let x = p1("../input/day03");
+
+    // let p1 = answerp1("../input/day02");
+    // let p2 = answerp2("../input/day02");
+    println!("{}", x);
+    // println!("{}", p2);
+    // assert_eq!(p1, 54641809925);
+    // assert_eq!(p2, 73694270688);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut joltage = "".to_string();
+        largest_joltage("987654321111111", &mut -1, &mut joltage);
+        assert_eq!(joltage, "98");
+    }
+
+    #[test]
+    fn test2() {
+        let mut joltage = "".to_string();
+        largest_joltage("811111111111119", &mut -1, &mut joltage);
+        assert_eq!(joltage, "89");
+    }
+
+    #[test]
+    fn test3() {
+        let mut joltage = "".to_string();
+        largest_joltage("234234234234278", &mut -1, &mut joltage);
+        assert_eq!(joltage, "78");
+    }
+
+    #[test]
+    fn test4() {
+        let mut joltage = "".to_string();
+        largest_joltage("818181911112111", &mut -1, &mut joltage);
+        assert_eq!(joltage, "92");
+    }
+    
+    #[test]
+    fn test5() {
+        let mut joltage = "".to_string();
+        largest_joltage("9191", &mut -1, &mut joltage);
+        assert_eq!(joltage, "99");
+    }
+
+        #[test]
+    fn test7() {
+        let mut joltage = "".to_string();
+        largest_joltage("1188", &mut -1, &mut joltage);
+        assert_eq!(joltage, "88");
+    }
+    
+    #[test]
+    fn test6() {
+        let sum = p1("example");
+        assert_eq!(sum, 357);
+    }
+}
+
