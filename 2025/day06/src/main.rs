ac845ab28656f13d7a428b6ffcbc93ae4c3150d3
MennoSt <mennosteensma@gmail.com>
2025-12-06 11:18:42 +0100

    rewrite a bit

--- 2025/day06/src/main.rs
+++ 2025/day06/src/main.rs
@@ -88,80 +88,57 @@ fn p1(input: &str) -> i128 {
 fn p2(input: &str) -> i128 {
     let operations = parse_data(input);
     let mut sum = 0;
-    for o in operations {
-
+    for o in &operations {
+        let new_vec = transpose_vec(o.values.clone(), o.operation.clone());
+        let mut answer=0;
         if o.operation == Operate::Added {
-            let new_vec = transpose_vec(o.values);
-            let answer: i128 = new_vec.iter().sum();
-            // println!("{}", answer);
-            sum += answer;
+            answer = new_vec.iter().sum();
         } else {
-            let new_vec = transpose_vec_mult(o.values);
-            let answer: i128 = new_vec.iter().product();
-            // println!("{}", answer);
-            sum += answer;
+            answer = new_vec.iter().product();
         }
+        sum += answer;
     }
     sum
 }
 
-fn transpose_vec(values: Vec<i128>) -> Vec<i128> {
+fn transpose_vec(values: Vec<i128>, operation: Operate) -> Vec<i128> {
     let mut transposed_values: Vec<i128> = Vec::new();
-
     let max = values.iter().max().unwrap().to_string().len();
 
-    for i in 0..max +2 {
+    for i in 0..max {
         let mut new_str: String = "".to_string();
         for val in &values {
             let str = val.to_string();
-            if let Some(c) = str.chars().nth(i) {
-                new_str.push(c);
-                // println!("The {}-th character is '{}'", i, c);
+            if operation == Operate::Added {
+                if let Some(c) = str.chars().nth(i) {
+                    new_str.push(c);
+                }
             } else {
-                // println!("String is too short");
+                if let Some(c) = str.chars().rev().nth(i) {
+                    new_str.push(c);
+                }
             }
         }
-        if !new_str.is_empty()
-        {
+        if !new_str.is_empty() {
             let newstr_val = new_str.parse::<i128>().unwrap();
             transposed_values.push(newstr_val);
         }
     }
 
-    transposed_values
-}
-
-fn transpose_vec_mult(values: Vec<i128>) -> Vec<i128> {
-    let mut transposed_values: Vec<i128> = Vec::new();
-
-    let max = values.iter().max().unwrap().to_string().len();
-
-    for i in 0..max+2 {
-        let mut new_str: String = "".to_string();
-        for val in &values {
-            let mut str = val.to_string();
-            if let Some(c) = str.chars().rev().nth(i) {
-                new_str.push(c);
-                // println!("The {}-th character is '{}'", i, c);
-            } else {
-                // println!("String is too short");
-            }
-        }
-        if !new_str.is_empty()
-        {
-            let newstr_val = new_str.parse::<i128>().unwrap();
-            transposed_values.push(newstr_val);
-        }
+    if transposed_values.len() > 4 {
+        panic!("HELP");
     }
 
     transposed_values
 }
 
+// 12866637663746 too high
 fn main() {
     let start = Instant::now();
 
     let answerp1 = p1(INPUT);
     println!("{}", answerp1);
+    assert_eq!(answerp1, 7644505810277);
     let answerp2 = p2(INPUT);
     println!("{}", answerp2);
     let duration = start.elapsed();
@@ -170,8 +147,6 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use std::f32::consts::E;
-
     use super::*;
     static INPUT_EXAMPLE: &str = "example";
 
@@ -186,29 +161,42 @@ mod tests {
         let sum = p2(INPUT_EXAMPLE);
         assert_eq!(sum, 3263827);
     }
-    #[test]
-    fn test3() {
-        let vec: Vec<i128> = vec![64, 23, 314];
+    // #[test]
+    // fn test3() {
+    //     let vec: Vec<i128> = vec![64, 23, 314];
 
-        let mut transposed = transpose_vec(vec);
-        let mut expected_vec = vec![4, 431, 623];
+    //     let mut transposed = transpose_vec(vec, Operate::Added);
+    //     let mut expected_vec = vec![4, 431, 623];
 
-        transposed.sort();
-        expected_vec.sort();
+    //     transposed.sort();
+    //     expected_vec.sort();
 
-        assert_eq!(transposed, expected_vec);
-    }
+    //     assert_eq!(transposed, expected_vec);
+    // }
 
-    #[test]
-    fn test4() {
-        let vec: Vec<i128> = vec![123, 45, 6];
+    // #[test]
+    // fn test4() {
+    //     let vec: Vec<i128> = vec![55, 32, 38, 99];
 
-        let mut transposed = transpose_vec_mult(vec);
-        let mut expected_vec = vec![1, 24, 356];
+    //     let mut transposed = transpose_vec(vec, Operate::Multiplied);
+    //     let mut expected_vec = vec![5339, 5289];
 
-        transposed.sort();
-        expected_vec.sort();
+    //     transposed.sort();
+    //     expected_vec.sort();
 
-        assert_eq!(transposed, expected_vec);
-    }
+    //     assert_eq!(transposed, expected_vec);
+    // }
+
+    // #[test]
+    // fn test5() {
+    //     let vec: Vec<i128> = vec![2222, 1111, 3555,355];
+
+    //     let mut transposed = transpose_vec(vec, Operate::Multiplied);
+    //     let mut expected_vec = vec![111];
+
+    //     transposed.sort();
+    //     expected_vec.sort();
+
+    //     assert_eq!(transposed, expected_vec);
+    // }
 }
