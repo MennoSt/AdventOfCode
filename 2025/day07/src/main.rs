5cb2cc8e07f7785fcfcb3e81055d9798b380a4c9
MennoSt <mennosteensma@gmail.com>
2025-12-07 19:00:44 +0100

    day 7 part 2

--- 2025/day07/src/main.rs
+++ 2025/day07/src/main.rs
@@ -8,6 +8,13 @@ static INPUT: &str = "../input/day07";
 fn p1(input: &str) -> i32 {
     let grid_init = filereader::_input_into_grid(input);
     let mut grid = grid_init.clone();
+    let splits = iterate_manifold(&mut grid);
+
+    grid._print();
+    splits
+}
+
+fn iterate_manifold(grid: &mut lib::grid::Grid) -> i32 {
     let mut splits = 0;
     for y in 0..grid._height() as i32 {
         for x in 0..grid._width() as i32 {
@@ -24,18 +31,64 @@ fn p1(input: &str) -> i32 {
             }
         }
     }
-
-    grid._print();
     splits
 }
 
+fn p2(input: &str) -> i128 {
+    let grid_init = filereader::_input_into_grid(input);
+    let mut grid = grid_init.clone();
+    let splits = iterate_manifold(&mut grid);
+    let mut count_grid = grid_init._create_visiter_grid();
+
+    for y in 0..grid._height() as i32 {
+        for x in 0..grid._width() as i32 {
+            let current_elem = grid._elem(x, y);
+            let up_elem = grid._up(x, y);
+            let x = x as i128;
+            let y = y as i128;
+
+            if current_elem == "^" && up_elem == "|" {
+                let count = count_grid._elem(x, y-1);
+
+                let countnew = count_grid._elem(x+1,y);
+                let new_val = count+countnew;
+                count_grid._set(x+1, y, new_val);
+
+
+                let countnew = count_grid._elem(x-1,y);
+                let new_val = count+countnew;
+                count_grid._set(x-1, y, new_val);
+            }
+            if up_elem == "|" {
+                let count = count_grid._elem(x, y-1);
+                let countnew = count_grid._elem(x,y);
+                let new_val = count + countnew;
+                count_grid._set(x, y, new_val);
+            }
+            
+            if up_elem == "S"
+            {
+                count_grid._set(x, y, 1);
+            }
+        }
+    }
+
+    count_grid._print();
+    grid._print();
+
+    let vectors = count_grid.get_vec();
+    let sum:i128 = vectors.last().unwrap().iter().sum();
+    sum
+}
 
 fn main() {
     let start = Instant::now();
 
     let part1 = p1(INPUT);
     println!("{}", part1);
-    // let part2 = p2(INPUT);
+    assert_eq!(part1, 1541);
+    let part2 = p2(INPUT);
+    println!("{}", part2);
 
     // utils::answer((part1,1363),(part2, 8184));
 
@@ -52,5 +105,10 @@ mod tests {
     fn test1() {
         let sum = p1(INPUT_EXAMPLE);
         assert_eq!(sum, 21);
+    }
+    #[test]
+    fn test2() {
+        let sum = p2(INPUT_EXAMPLE);
+        assert_eq!(sum, 40);
     }
 }
--- lib/rustutils/src/grid.rs
+++ lib/rustutils/src/grid.rs
@@ -135,10 +135,10 @@ impl Grid {
         Ok(())
     }
 
-    pub fn _create_visiter_grid(&self) -> Gridi32 {
+    pub fn _create_visiter_grid(&self) -> Gridi128 {
         let rows = self._width();
         let cols = self._height();
-        Gridi32 {
+        Gridi128 {
             grid_vec: vec![vec![0; rows]; cols],
         }
     }
@@ -150,6 +150,10 @@ pub struct Gridi32 {
 }
 
 impl Gridi32 {
+    pub fn get_vec(&self) -> Vec<Vec<i32>> {
+        return self.grid_vec.clone();
+    }
+    
     pub fn _height(&self) -> usize {
         return self.grid_vec.len();
     }
@@ -205,6 +209,76 @@ impl Gridi32 {
     }
 
     pub fn _max(&self) -> Option<i32> {
+        let max_value = self.grid_vec.iter().flatten().max().cloned();
+
+        return max_value;
+    }
+}
+
+pub struct Gridi128 {
+    pub grid_vec: Vec<Vec<i128>>,
+}
+
+impl Gridi128 {
+    pub fn get_vec(&self) -> Vec<Vec<i128>> {
+        return self.grid_vec.clone();
+    }
+    
+    pub fn _height(&self) -> usize {
+        return self.grid_vec.len();
+    }
+
+    pub fn _width(&self) -> usize {
+        return self.grid_vec[0].len();
+    }
+
+    pub fn _elem(&self, x: i128, y: i128) -> i128 {
+        if x < 0 || y < 0 {
+            return 0;
+        }
+
+        let x_usize: usize = x as usize;
+        let y_usize: usize = y as usize;
+
+        if x_usize > (self._width() - 1) || y_usize > (self._height() - 1) {
+            return 0;
+        }
+
+        return self.grid_vec[y_usize][x_usize].clone();
+    }
+
+    pub fn _set(&mut self, x: i128, y: i128, value: i128) {
+        let x_usize: usize = x as usize;
+        let y_usize: usize = y as usize;
+        self.grid_vec[y_usize][x_usize] = value;
+    }
+
+    pub fn _print(&self) {
+        for grid in &self.grid_vec {
+            for val in grid {
+                print!("{:7}", val);
+            }
+            println!();
+        }
+    }
+
+    pub fn _print_special(&self) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .append(true) // Open for appending
+            .create(true) // Create the file if it doesn't exist
+            .open("example.txt")?; // Open the file
+
+        writeln!(file)?;
+        for grid in &self.grid_vec {
+            let result: String = grid.iter().map(|num| num.to_string()).collect();
+            // fs::write("output.txt",result);
+            writeln!(file, "{:7}", result)?;
+        }
+        writeln!(file, "")?;
+        Ok(())
+    }
+
+    pub fn _max(&self) -> Option<i128> {
         let max_value = self.grid_vec.iter().flatten().max().cloned();
 
         return max_value;
