104e384cf3f0a799140bf8dfaaa23bf09517f97a
Menno Steensma <ext.menno.steensma@swarco.com>
2025-12-08 18:41:57 +0100

    updated stuff

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -1,6 +1,7 @@
 use lib::filereader;
 use lib::utils;
 use lib::utils::Coordinate;
+use ordered_float::OrderedFloat;
 use std::time::Instant;
 
 static INPUT: &str = "../input/day08";
@@ -14,8 +15,8 @@ struct Coordinate3D {
 
 #[derive(Debug, Clone, Copy)]
 struct JDistance {
-    pair : (Coordinate3D, Coordinate3D),
-    distance : f64,
+    pair: (Coordinate3D, Coordinate3D),
+    distance: f64,
 }
 
 fn parse_data(input: &str) -> Vec<Coordinate3D> {
@@ -39,9 +40,61 @@ fn calculate_distance(coordinate1: &Coordinate3D, coordinate2: &Coordinate3D) ->
     (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt()
 }
 
-fn p1(input: &str) -> i32 {
+fn p1(input: &str) -> i128 {
     let coordinates = parse_data(input);
+    let mut jdistances = calculate_jdistances(coordinates);
+    jdistances.sort_by_key(|item| OrderedFloat(item.distance));
 
+    let mut chains: Vec<Vec<Coordinate3D>> = Vec::new();
+
+    for d in jdistances.iter().take(1000) {
+        let c1 = d.pair.0;
+        let c2 = d.pair.1;
+
+        let mut found_chain = false;
+        for chain in chains.iter_mut() {
+            if chain.contains(&c1) && chain.contains(&c2) {
+                found_chain = true;
+                break;
+            } else if chain.contains(&c1) || chain.contains(&c2) {
+                chain.push(c1);
+                chain.push(c2);
+                chain.sort_by_key(|p| p.x);
+                chain.dedup_by(|a, b| a.x == b.x);
+                found_chain = true;
+                break;
+            }
+        }
+
+        if chains.is_empty() || found_chain == false {
+            let mut chain: Vec<Coordinate3D> = Vec::new();
+            chain.push(c1);
+            chain.push(c2);
+            chains.push(chain)
+        }
+    }
+
+    for chain in &mut chains {
+        chain.sort_by_key(|p| p.x);
+    }
+
+    for chain in &mut chains {
+        chain.dedup_by(|a, b| a.x == b.x);
+
+        println!("lenght: {} coordinate {:?}", chain.len(), chain);
+    }
+
+    let product: usize = chains
+        .iter()
+        .map(|chain| chain.len())
+        .fold(1, |acc, len| acc * len);
+
+    // println!("{:?}", chains);
+
+    product as i128
+}
+
+fn calculate_jdistances(coordinates: Vec<Coordinate3D>) -> Vec<JDistance> {
     let n = coordinates.len();
 
     let pairs: Vec<_> = (0..n)
@@ -51,18 +104,15 @@ fn p1(input: &str) -> i32 {
         })
         .collect();
 
-    println!("{:?}", pairs);
-    println!("size: {}", pairs.len());
-
     let mut jdistances: Vec<JDistance> = Vec::new();
     for pair in pairs {
         let distance = calculate_distance(pair.0, pair.1);
-        jdistances.push(JDistance{pair:(pair.0.clone(),pair.1.clone()), distance:distance});
+        jdistances.push(JDistance {
+            pair: (pair.0.clone(), pair.1.clone()),
+            distance: distance,
+        });
     }
-
-    println!("{:?}", jdistances);
-
-    0
+    jdistances
 }
 
 fn main() {
@@ -70,7 +120,7 @@ fn main() {
 
     let part1 = p1(INPUT);
     println!("{}", part1);
-    assert_eq!(part1, 1541);
+    // assert_eq!(part1, 1541);
     // let part2 = p2(INPUT);
     // println!("{}", part2);
 
